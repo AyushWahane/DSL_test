3a23471fa5dc8f57f8f14c225284482d7a60a09f
AyushWahane <83772772+AyushWahane@users.noreply.github.com>
2022-12-02 01:34:02 +0530

    Create Build.groovy

---
+++ Major_jobs/Build.groovy
@@ -0,0 +1,63 @@
+pipelineJob('Build') {
+    description('Product ecom build job')
+    triggers {
+        upstream('webhook', 'UNSTABLE')
+    }
+    configure { it / 'triggers' / 'com.cloudbees.jenkins.GitHubPushTrigger' / 'spec' }
+    definition {
+        cps {
+            script('''pipeline {
+    environment {
+        registry = "krishna1708/minor_test1"
+        registryCredential = ''
+        dockerImage = ''
+    }
+    agent {
+    node {
+        label 'slave'
+        }
+    }
+    stages {
+        stage('Clone Git Repository') {
+            steps{
+                sh """
+                    rm -rf ./product-major
+                    git clone --branch master https://github.com/AyushWahane/product-major.git
+                    
+                """
+            }
+        }   
+        stage('Building image') {
+            steps{
+                sh """
+                    cd product-major
+                    sudo docker login -u krishna1708 -p Abcd97@4321
+                    sudo docker build -t krishna1708/product-major:latest .
+                """
+            }
+        }
+        
+        
+        stage('Push image') {
+            steps{
+                sh """
+                    sudo docker login -u krishna1708 -p Abcd97@4321
+                    sudo docker push krishna1708/product-major:latest
+                """
+            }
+        }
+        
+        stage('Cleaning up') {
+            steps{
+                sh """
+                    sudo docker rmi krishna1708/product-major:latest
+                """
+            }
+        }
+    }
+}
+            ''')
+            sandbox()
+        }
+    }
+}
