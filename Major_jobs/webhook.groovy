646809d5f507c86dbbd27c3a4d85b078dcdec318
AyushWahane <83772772+AyushWahane@users.noreply.github.com>
2022-12-02 01:42:55 +0530

    Create webhook.groovy

---
+++ Major_jobs/webhook.groovy
@@ -0,0 +1,19 @@
+job('webhook') {
+    scm {
+        git {
+            remote {
+                url('https://github.com/AyushWahane/product-major.git')
+                branch('*/master')
+            }
+        }
+    }
+    triggers {
+        scm('* * * * *')
+    }
+    configure { it / 'triggers' / 'com.cloudbees.jenkins.GitHubPushTrigger' / 'spec' }
+    steps{
+        shell(
+            '''pwd'''
+        )
+    }
+}
